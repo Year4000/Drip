09e4ad41b74ca60d11db85fdf3515c7aec92d724
Joshua Rodriguez <ewized@gmail.com>
2016-02-10 11:29:05 -0800

    Have DripDriver load durring start up with Constants file

--- src/main/java/net/year4000/drip/Constants.java
+++ src/main/java/net/year4000/drip/Constants.java
@@ -1,4 +1,17 @@
+/*
+ * Copyright 2016 Year4000. All Rights Reserved.
+ */
+
 package net.year4000.drip;
 
-public class Constants {
+public final class Constants {
+
+    /** The version of Drip */
+    public static final String VERSION = "${version}";
+
+    /** The git hash for this version of Drip */
+    public static final String GIT_HASH = "${git_hash}";
+
+    /** The full version of Drip contains version and git hash */
+    public static final String FULL_VERSION = "${version}-${git_hash}";
 }
--- src/main/java/net/year4000/drip/DripDriver.java
+++ src/main/java/net/year4000/drip/DripDriver.java
@@ -1,4 +1,11 @@
+/*
+ * Copyright 2016 Year4000. All Rights Reserved.
+ */
+
 package net.year4000.drip;
 
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "net.year4000.drip", name = "Drip", version = Constants.FULL_VERSION)
 public class DripDriver {
 }
