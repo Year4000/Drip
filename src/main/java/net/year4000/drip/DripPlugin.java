7432878ecfda6c8f217ccd0419c65024cf7d2390
Joshua Rodriguez <ewized@gmail.com>
2016-03-05 17:58:19 -0800

    Update utilities and let drip handle it

--- src/main/java/net/year4000/drip/DripPlugin.java
+++ src/main/java/net/year4000/drip/DripPlugin.java
@@ -4,8 +4,14 @@
 
 package net.year4000.drip;
 
+import net.year4000.utilities.sponge.AbstractSpongePlugin;
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id = "net.year4000.drip", name = "Drip", version = Constants.FULL_VERSION)
-public class DripPlugin {
+@Plugin(id = "drip", name = "Drip", version = Constants.FULL_VERSION)
+public class DripPlugin extends AbstractSpongePlugin {
+
+    /** Get the instance of this plugin */
+    public static DripPlugin get() {
+        return instance();
+    }
 }
