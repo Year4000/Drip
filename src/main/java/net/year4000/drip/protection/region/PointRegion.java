37227f0ddf40094f769cea477129c9c213bb29ff
Joshua Rodriguez <ewized@gmail.com>
2016-04-08 09:13:13 -0700

    Region stuff wip

---
+++ src/main/java/net/year4000/drip/protection/region/PointRegion.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Year4000. All Rights Reserved.
+ */
+
+package net.year4000.drip.protection.region;
+
+import com.flowpowered.math.vector.Vector3i;
+import com.google.common.collect.ImmutableSet;
+import net.year4000.utilities.ObjectHelper;
+
+import java.util.Optional;
+import java.util.Set;
+
+/** A region that contains a single point */
+public final class PointRegion implements Region {
+    private Vector3i point;
+
+    /** Allow creation of a PointRegion via x,y,z cords */
+    public PointRegion(int x, int y, int z) {
+        this(new Vector3i(x, y, z));
+    }
+
+    /** Create the region with vector3i point */
+    public PointRegion(Vector3i point) {
+        this.point = ObjectHelper.nonNull(point, "point");
+    }
+
+    @Override
+    public boolean contains(Vector3i vector3i) {
+        return point.equals(vector3i);
+    }
+
+    @Override
+    public Optional<Set<Vector3i>> getPoints() {
+        return Optional.of(ImmutableSet.of(point));
+    }
+}
---
+++ src/main/java/net/year4000/drip/protection/region/Region.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016 Year4000. All Rights Reserved.
+ */
+
+package net.year4000.drip.protection.region;
+
+import com.flowpowered.math.vector.Vector3i;
+import org.spongepowered.api.entity.Entity;
+
+import java.util.Optional;
+import java.util.Set;
+
+/** Region interface that is the base of the region */
+public interface Region {
+    /** Get a set of points the region contains, or if its not possible return empty */
+    Optional<Set<Vector3i>> getPoints();
+
+    /** Check when the vector is in the region */
+    boolean contains(Vector3i vector3i);
+
+    /** Does this region contain the entity */
+    default boolean contains(Entity entity) {
+        return contains(entity.getLocation().getPosition().toInt());
+    }
+}
